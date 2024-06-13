ecb070a6cb71fd5ea7c2cbfe2d650a740ab4fe61
it <kyle9742@naver.com>
2024-06-13 15:54:56 +0900

    파라미터를 통해 레시피 디테일

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,11 +1,19 @@
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch';
+
 import './Recipe.css'
 
-const Recipe = () => {
+
+export default function Recipe() {
+  const {id} = useParams();
+  const url = 'http://localhost:3030/recipe/' + id;
+  const {error, isPending, data:recipe} = useFetch(url);
+
   return (
-    <div>
-      레시피
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>로딩중...</p>}
+      {recipe && <h1>{recipe.title}</h1>}
     </div>
   )
 }
-
-export default Recipe
