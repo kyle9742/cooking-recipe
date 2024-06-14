365fcce0278c758f7cab59685e59195b9a87e452
it <kyle9742@naver.com>
2024-06-14 12:23:09 +0900

    서치바 검색 기능

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,11 +1,24 @@
-import './Search.css'
+import RecipeList from "../../components/RecipeList";
+import { useFetch } from "../../hooks/useFetch";
+import { useLocation } from "react-router-dom";
+import "./Search.css";
 
 const Search = () => {
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+
+  const url = "http://localhost:3030/recipes?q=" + query;
+  const { data, isPending, error } = useFetch(url);
+
   return (
     <div>
-      서치
+      <h2 className="page-title">"{query}"를 포함하는 레시피는 </h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">로딩중...</p>}
+      {data && <RecipeList recipes={data} />}
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
